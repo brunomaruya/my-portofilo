20b7988fb45d10b8679c6d92f484958f49440d55
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-23 21:36:58 +0900

    Remove Navbar component from layout folder

--- components/layout/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { CircularProgress } from "@nextui-org/react";
-export default function Navbar() {
-  return (
-    <div>
-      <CircularProgress />
-    </div>
-  );
-}
