e8f7c0ff7363927893d98394f430ec39644b77ab
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-23 21:49:59 +0900

    Changed NavbarBrand text and added new NavbarItems

--- components/layout/NavbarComp.tsx
+++ components/layout/NavbarComp.tsx
@@ -30,24 +30,23 @@ export default function NavbarComp() {
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
-        <NavbarBrand>Bm.</NavbarBrand>
+        <NavbarBrand className="text-primary text-3xl font-bold">
+          Bm.
+        </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="end">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
+          <Link href="/">Home</Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
+          <Link href="#">About</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#">Projects</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#">Resume</Link>
         </NavbarItem>
       </NavbarContent>
 
