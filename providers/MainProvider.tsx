8f59ac4409f33bf895cced0d8488d9f9cddb007e
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-23 22:19:55 +0900

    Added NextThemesProvider to MainProvider
    Added NextThemesProvider to MainProvider to enable theme switching functionality.

--- providers/MainProvider.tsx
+++ providers/MainProvider.tsx
@@ -1,11 +1,18 @@
 "use client";
 import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function MainProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
