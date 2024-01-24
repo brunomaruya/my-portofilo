3066e0537a0700f771a9f772fea9b3f5f985dba9
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-24 10:42:26 +0900

    Added LinkedIn logo and updated dependencies
    Added LinkedIn logo to the "Find Me" section and updated the "@phosphor-icons/react" dependency to version 2.0.15 in package.json and package-lock.json.

--- components/pages/home/FindMe.tsx
+++ components/pages/home/FindMe.tsx
@@ -1,21 +1,25 @@
+"use client";
+
+import { LinkedinLogo } from "@phosphor-icons/react";
 import Link from "next/link";
 import React from "react";
 
 export default function FindMe() {
   return (
-    <div className="mt-10">
-      {/* <div className="flex flex-col items-center">
+    <div className="mt-20">
+      <div className="flex flex-col items-center">
         <h1 className="text-5xl mb-3">FIND ME ON</h1>
-        <h6 className="text-lg">Fell free to connect with me</h6>
+        <h6 className="text-lg">
+          Fell free to <span className="text-primary">connect</span> with me
+        </h6>
         <ul>
           <li>
-            <Link
-              rel="stylesheet"
-              href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
-            />
+            <Link href="#">
+              <LinkedinLogo size={32} />
+            </Link>
           </li>
         </ul>
-      </div> */}
+      </div>
     </div>
   );
 }
