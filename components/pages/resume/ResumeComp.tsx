62df47c0850436034e79613d7cf9cde1e56521ad
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-24 22:18:02 +0900

    Add download functionality for resumes
    Added a download button to each resume tab in the ResumeComp component. The download button uses the react-use-downloader library to download the resume file when clicked.

--- components/pages/resume/ResumeComp.tsx
+++ components/pages/resume/ResumeComp.tsx
@@ -4,20 +4,44 @@ import Image from "next/image";
 import englishResume from "../../../public/resumes/resume.jpg";
 import japaneseResume from "../../../public/resumes/rirekisho1.jpg";
 import japaneseResume2 from "../../../public/resumes/rirekisho2.jpg";
+import { Button } from "@nextui-org/react";
 
 import React from "react";
+import useDownloader from "react-use-downloader";
 
 export default function ResumeComp() {
+  const { download } = useDownloader();
+  //TODO: update resumes
   const resumes = [
-    { language: "Japanese", images: [japaneseResume, japaneseResume2] },
-    { language: "English", images: [englishResume] },
+    {
+      language: "Japanese",
+      images: [japaneseResume, japaneseResume2],
+      fileUrl: "",
+      filename: "履歴書",
+    },
+    {
+      language: "English",
+      images: [englishResume],
+      fileUrl: "",
+      filename: "Resume",
+    },
   ];
 
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex w-full flex-col  ">
       <Tabs aria-label="Options">
-        {resumes.map((resume, index) => (
-          <Tab key={resume.language} title={resume.language}>
+        {resumes.map((resume) => (
+          <Tab
+            key={resume.language}
+            title={resume.language}
+            className="flex flex-col items-center"
+          >
+            <Button
+              className="bg-accent mb-5"
+              onClick={() => download(resume.fileUrl, resume.filename)}
+            >
+              Download
+            </Button>
             {resume.images.map((image, index) => (
               <Image
                 key={index}
