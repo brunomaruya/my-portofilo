75c3b2bdfd669fe3eea950726b8515884aaf6045
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-24 10:31:58 +0900

    Added FindMe component to the Home page
    Added the FindMe component to the Home page to display contact information.

--- components/pages/home/FindMe.tsx
+++ components/pages/home/FindMe.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 export default function FindMe() {
-  return <div>FindMe</div>;
+  return (
+    <div className="mt-10">
+      {/* <div className="flex flex-col items-center">
+        <h1 className="text-5xl mb-3">FIND ME ON</h1>
+        <h6 className="text-lg">Fell free to connect with me</h6>
+        <ul>
+          <li>
+            <Link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+            />
+          </li>
+        </ul>
+      </div> */}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "../../components/pages/home/Hero";
 import Introduce from "../../components/pages/home/Introduce";
+import FindMe from "../../components/pages/home/FindMe";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Introduce />
+      <FindMe />
     </>
   );
 }
