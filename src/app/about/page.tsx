d8bf8408d6ff48d9c5901348ab879f18102e96b4
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-24 21:24:46 +0900

    Delete DaysICode

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import AboutComp from "../../../components/pages/about/AboutComp";
 import SkillSet from "../../../components/pages/about/SkillSet";
-import DaysICode from "../../../components/pages/about/DaysICode";
 
 export default function About() {
   return (
     <div className="flex flex-col gap-20 justify-between">
       <AboutComp />
       <SkillSet />
-      <DaysICode />
     </div>
   );
 }
