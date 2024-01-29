76b745418991890c2129c136f86cd4c182697f00
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-29 22:13:16 +0900

    Added LinkedIn contact link and icon to the Footer
    Added LinkedIn contact link and icon to the Footer component in order to display the LinkedIn logo and link to Bruno Maruya's LinkedIn profile. The contact information is fetched from the `contacts` constant in the `constants/contacts.tsx` file.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { LinkedinLogo } from "@phosphor-icons/react";
 import React from "react";
+import { contacts } from "../../constants/contacts";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="   bg-black mt-32  flex flex-col sm:flex-row gap-3 items-center justify-between px-20 py-3">
       <div className="text-sm sm:text-md">Bruno Maruya</div>
       <div>
-        <ul>
-          <li>
-            <LinkedinLogo size={32} />
-          </li>
-        </ul>
+        {contacts.map((contact, index) => (
+          <div className="text-3xl" key={index}>
+            <Link href={contact.link} target="_blank">
+              {contact.icon}
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ constants/contacts.tsx
@@ -0,0 +1,8 @@
+import { LinkedinLogo } from "@phosphor-icons/react";
+
+export const contacts = [
+  {
+    link: "https://www.linkedin.com/in/bruno-maruya/",
+    icon: <LinkedinLogo />,
+  },
+];
