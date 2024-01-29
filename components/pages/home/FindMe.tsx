771945df7e9a7e978eb4735b1f9d63803aec86d2
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-30 00:45:25 +0900

    Fix typo in FindMe component
    Corrected a typo in the text of the FindMe component, changing "Fell" to "Feel" for better readability.

--- components/pages/home/FindMe.tsx
+++ components/pages/home/FindMe.tsx
@@ -11,7 +11,7 @@ export default function FindMe() {
       <div className="flex flex-col items-center">
         <h1 className="text-5xl mb-3">FIND ME ON</h1>
         <h6 className="text-lg">
-          Fell free to <span className="text-primary">connect</span> with me
+          Feel free to <span className="text-primary">connect</span> with me
         </h6>
         <ul className="mt-8 flex gap-3">
           {contacts.map((contact, index) => (
