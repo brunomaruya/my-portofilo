19ff4549ddd2976690f99b385dedd124a128abbc
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-29 21:22:00 +0900

    Change footer credit to "Bruno Maruya"
    The commit message should be: "Update footer credit"
    The description should be: "Changed the footer credit from 'Design inspired by Soumyajit Behera' to 'Bruno Maruya'"

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,9 +5,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="   bg-black mt-32  flex flex-col sm:flex-row gap-3 items-center justify-between px-20 py-3">
-      <div className="text-sm sm:text-md">
-        Design inspired by Soumyajit Behera
-      </div>
+      <div className="text-sm sm:text-md">Bruno Maruya</div>
       <div>
         <ul>
           <li>
