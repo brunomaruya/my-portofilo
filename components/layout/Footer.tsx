ae6b7e3fbe91f56c1ad48a4e23ee4fca8ef7f230
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-02-08 19:20:02 +0900

    Refactored contact section in Footer
    Refactored the contact section in the Footer component to improve readability and maintainability. Moved the email address to a separate div and added a click event to open the default email client when clicked.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,14 +8,23 @@ export default function Footer() {
   return (
     <div className="   bg-black mt-32  flex flex-col sm:flex-row gap-3 items-center justify-between px-20 py-3">
       <div className="text-sm sm:text-md">Bruno Maruya</div>
-      <div className="flex gap-3">
-        {contacts.map((contact, index) => (
-          <div className="text-3xl" key={index}>
-            <Link href={contact.link} target="_blank">
-              {contact.icon}
-            </Link>
-          </div>
-        ))}
+      <div className="flex flex-col md:flex-row items-center gap-3">
+        <div
+          onClick={() =>
+            (window.location.href = "mailto:brun0yuj1ym02@gmail.com")
+          }
+        >
+          brun0yuj1ym02@gmail.com
+        </div>
+        <div className="flex gap-3">
+          {contacts.map((contact, index) => (
+            <div className="text-3xl" key={index}>
+              <Link href={contact.link} target="_blank">
+                {contact.icon}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
