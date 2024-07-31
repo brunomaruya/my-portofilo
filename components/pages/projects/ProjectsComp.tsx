56d292451c34729fc73140f8ee523af9265716e9
bruno maruya <brun0yuj1ym02@gmail.com>
2024-07-31 10:38:43 +0900

    update projects page

--- components/pages/projects/ProjectsComp.tsx
+++ components/pages/projects/ProjectsComp.tsx
@@ -39,32 +39,34 @@ export default function ProjectsComp() {
       <h6 className="mt-5 lg:text-xl mb-10">Here are a few of my projects</h6>
       <div className="flex flex-wrap justify-center gap-7">
         {projects.map((project, index) => (
-          <Card
-            key={index}
-            className="flex flex-col items-center justify-between  border border-primary w-96 bg-background rounded-md p-5 md:p-10"
-          >
-            <div>
-              <Image
-                className="h-40 w-80 object-cover object-top  mb-5"
-                width={500}
-                height={500}
-                alt="image"
-                src={project.image}
-              />
+          <Link href={project.link} target="_blank">
+            <Card
+              key={index}
+              className="flex flex-col items-center justify-between  border border-primary w-96 bg-background rounded-md p-5 md:p-10"
+            >
+              <div>
+                <Image
+                  className="h-40 w-80 object-cover object-top  mb-5"
+                  width={500}
+                  height={500}
+                  alt="image"
+                  src={project.image}
+                />
 
-              <h1 className="text-xl mb-3">{project.title}</h1>
-              <p>{project.description}</p>
-            </div>
+                <h1 className="text-xl mb-3">{project.title}</h1>
+                <p>{project.description}</p>
+              </div>
 
-            <div className="flex gap-3 mt-3 ">
-              <Link href={project.link} target="_blank">
-                <CgGlobeAlt className="text-2xl" />
-              </Link>
-              <Link href={project.github} target="_blank">
-                <GithubLogo className="text-2xl" />
-              </Link>
-            </div>
-          </Card>
+              <div className="flex gap-3 mt-3 ">
+                <Link href={project.link} target="_blank">
+                  <CgGlobeAlt className="text-2xl" />
+                </Link>
+                <Link href={project.github} target="_blank">
+                  <GithubLogo className="text-2xl" />
+                </Link>
+              </div>
+            </Card>
+          </Link>
         ))}
       </div>
     </div>
